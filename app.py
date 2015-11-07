dd5792bef40a8d4a162036067af49f21ccef56d1
Chloe Kaubisch <nemononiam@gmail.com>
2015-11-07 09:56:39 -0500

    app backend

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-# the app will be here
-# why is it in python
-# isn't it going to be jscript or html
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def begin():
+    return "HI"
+
+if __name__=="__main__":
+    app.debug = True
+    app.run('0.0.0.0', port = 8000)
