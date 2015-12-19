b22d1399b5690ca69836957bedea4534e133964e
asg17864 <agranger17864@gmail.com>
2015-12-19 10:30:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var http = require ("http");
 
 var server = http.createServer (function (req, res) {
   res.writeHead (200);
-  res.end ("Temporary text");
+  res.end (data);
 });
 server.listen (8080);
