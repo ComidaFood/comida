08dfe668fb922bf15adab3ac9e884587fcb843c3
asg17864 <agranger17864@gmail.com>
2016-02-06 10:33:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-//app.js
+var template = require ("swig");
+var html = template.renderFile ("template.html", {
+	recipes: data
+});
 var http = require ("http");
-
 var server = http.createServer (function (req, res) {
-  res.writeHead (200);
-  res.end (String (data));
+	res.writeHead (2000);
+	res.end (html);
 });
 server.listen (3000);
